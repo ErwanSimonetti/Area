db48b028fe8e4b373fae6c1fe9e1f663fe96cf1c
Azzzen <axel.zenine@epitech.eu>
2022-10-06 11:54:05 +0200

    [FIX] removed superfluous component in wallet

--- web/web-app/src/Wallet.js
+++ web/web-app/src/Wallet.js
@@ -19,31 +19,11 @@ function RollingCarousel() {
   )
 }
 const services = ["Spotify", "Twitter", "Discord", "Github"];
+const actions = ["Un artiste poste un nouveau son", "J'ai ajouté une chanson à une playlist", "Un autre option pour laquelle j'ai pas d'idée"];
 
-function SimpleDialog({ onClose, selectedValue, open, title }) {
-  const handleClose = () => {
-    onClose(selectedValue);
-  };
-
-  const handleListItemClick = (value) => {
-    // onClose(value);
-  };
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>{title}</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        {services.map((service) => (
-          <ListItem button onClick={() => handleListItemClick(service)} key={service}>
-            <ListItemText primary={service} />
-          </ListItem>
-        ))}
-      </List>
-    </Dialog>
-  );
-}
 export function Wallet() {
-  const [open, setOpen] = React.useState(false);
-
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const newCard = null;
 
   const [cards, setCards] = React.useState([{
     actionName: "J'update une de mes playlists",
@@ -54,24 +34,76 @@ export function Wallet() {
 
 
   const handleNewAREA = () => {
-    setOpen(true);
+    setOpenDialog(true);
   };
   const handleClose = () => {
-    setOpen(false);
+    if (newCard !== null)
+      cards.push(newCard);
+    setOpenDialog(false);
   };
 
   return (
     <React.Fragment>
       <RollingCarousel />
-      <Button size="small" onClick={handleNewAREA}> nouvelle AREA </Button>
+      <Button size="small" onClick={() => { setOpenDialog(true); console.log("open") }}> nouvelle AREA </Button>
       <AREACard cards={cards} />
-      <SimpleDialog
-        open={open}
-        onClose={handleClose}
-        title={"Nouvelle AREA"}
-      />
-    </React.Fragment>
+      <NewCardDialog onClose={() => setOpenDialog(false)} open={openDialog} />
+    </React.Fragment >
   );
-
 }
+
+export function NewCardDialog(props) {
+
+  const [actionService, setActionService] = React.useState('');
+  const [action, setAction] = React.useState('');
+  const [reactionService, setReactionService] = React.useState('');
+  const [reaction, setReaction] = React.useState('');
+  const [openServiceActionDialog, setOpenServiceActionDialog] = React.useState(false);
+  const [openActionDialog, setOpenActionDialog] = React.useState(false);
+  const [openServiceReactionDialog, setOpenServiceReactionDialog] = React.useState(false);
+  const [openReactionDialog, setOpenReactionDialog] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      {/* Ce fdp de dialogue s'ouvre pas là jsp pourquoi ??? */}
+      <Dialog onClose={() => props.setOpenDialog(false)} open={props.openDialog}>
+        <DialogTitle>Nouvelle AREA</DialogTitle>
+      </Dialog>
+      {/* Service Action Pick */}
+      <Dialog onClose={() => setOpenServiceActionDialog(false)} open={openServiceActionDialog}>
+        <DialogTitle>Choisir un Service d'action</DialogTitle>
+        <List sx={{ pt: 0 }}>
+          {services.map((service) => (
+            <ListItem button onClick={() => setActionService(service)} key={service}>
+              <ListItemText primary={service} />
+            </ListItem>
+          ))}
+        </List>
+      </Dialog>
+      {/* Action Pick */}
+      <Dialog onClose={() => setOpenActionDialog(false)} open={openActionDialog}>
+        <DialogTitle>Choisir une action</DialogTitle>
+        <List sx={{ pt: 0 }}>
+          {actions.map((action) => (
+            <ListItem button onClick={() => setAction(action)} key={action}>
+              <ListItemText primary={action} />
+            </ListItem>
+          ))}
+        </List>
+      </Dialog>
+      {/* Service Reaction Pick */}
+      <Dialog onClose={() => setOpenServiceReactionDialog(false)} open={openServiceReactionDialog}>
+        <DialogTitle>Choisir un Service d'action</DialogTitle>
+        <List sx={{ pt: 0 }}>
+          {services.map((service) => (
+            <ListItem button onClick={() => setReactionService(service)} key={service}>
+              <ListItemText primary={service} />
+            </ListItem>
+          ))}
+        </List>
+      </Dialog>
+    </React.Fragment >
+  )
+}
+
 export default Wallet
