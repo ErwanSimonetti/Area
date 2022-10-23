eeaeb4a116a114ffe7f87b672f02f258929ee6ef
Azzzen <axel.zenine@epitech.eu>
2022-10-23 18:43:16 +0200

    feat(login): fixed login page google auth works, login page is now pretty again navbar goes back to the "homepage" (which is just links listed)

--- web/web-app/src/Components/Navbar.js
+++ web/web-app/src/Components/Navbar.js
@@ -1,16 +1,21 @@
 // import "./App.css"
+import * as React from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
-import { Toolbar, Typography, AppBar } from '@mui/material'
+import { Toolbar, Typography, AppBar, Button, Link } from '@mui/material'
 
 export default function NavBar () {
   return (
         <div>
-            <AppBar position="relative">
+            <AppBar position='relative'>
                 <Toolbar>
                     <MenuIcon sx={{ mr: 2 }} />
-                    <Typography variant="h6" color="inherit" noWrap>
-                        AREA
-                    </Typography>
+                        <Link href="/">
+                            <Button >
+                                <Typography variant='h6' color='white' noWrap>
+                                    AREA
+                                </Typography>
+                            </Button>
+                        </Link>
                 </Toolbar>
             </AppBar>
         </div>
