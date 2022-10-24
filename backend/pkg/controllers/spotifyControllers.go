6b44c708f98534a8fc7dbf85870ce9222df92beb
Geekosaure <juliette.destang@epitech.eu>
2022-10-24 10:17:09 +0200

    feat: (backend-frontend) oAuth2 with discord and first reaction (bot can write a message) - in Discord.js, add a button that redirecton the Discord oAuth. User can create a webhook on a specific server and channel. - in spotifyControllers.go, add a /auth/discord route that perfom the redirection. The function retrieve the token of the webhook, and display a message thanks to the bot added by the user.
    Co-authored-by: Timothee de Boynes <HKtueur1@users.noreply.github.com>

--- backend/pkg/controllers/spotifyControllers.go
+++ backend/pkg/controllers/spotifyControllers.go
@@ -6,13 +6,23 @@ import (
 	"log"
 	"io/ioutil"
 	"net/http"
+	// "bytes"
 	"net/url"
+	"strconv"
+	"strings"
+	"time"
+	// "AREA/pkg/utils"
+	// "github.com/gorilla/mux"
 	
-	"golang.org/x/oauth2"
-	"github.com/ravener/discord-oauth2"
+	// "golang.org/x/oauth2"
+	// "github.com/ravener/discord-oauth2"
 	"github.com/zmb3/spotify"
 	"github.com/jasonlvhit/gocron"
 	"golang.org/x/oauth2/clientcredentials"
+	// "github.com/gin-contrib/cors"
+	"encoding/json"
+    // "github.com/gin-gonic/gin"
+	"github.com/DisgoOrg/disgohook"
 )
 
 func ConnectionSpotify() {
@@ -83,53 +93,118 @@ var state = "random"
 
 func HelloDiscord(w http.ResponseWriter, r *http.Request){
 
-	conf := &oauth2.Config{
-		RedirectURL: "http://localhost:8080/auth/discord",
-		ClientID: "1033382176785432656",
-		ClientSecret: "XYlj-YTnAqQ-7HNes4I5xRTIdIKdLvyZ",
-		Scopes: []string{discord.ScopeActivitiesRead, discord.ScopeIdentify, discord.ScopeMessagesRead},
-		Endpoint: discord.Endpoint,
+	// conf := &oauth2.Config{
+	// 	RedirectURL: "http://localhost:8080/auth/discord",
+	// 	ClientID: "1033382176785432656",
+	// 	ClientSecret: "XYlj-YTnAqQ-7HNes4I5xRTIdIKdLvyZ",
+	// 	Scopes: []string{discord.ScopeWebhookIncoming },
+	// 	Endpoint: discord.Endpoint,
+	// }
+	// // token, _ := conf.Exchange(context.Background(), r.FormValue("code"))
+	// // fmt.Println(token)
+	// toekn = token
+	myurl := "https://discordapp.com/api/v6/oauth2/token";
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
 	}
 
-
-		data := url.Values{
-			"content":       {"John Doe"},
-			"tts": {"false"},
-		}
-
-
-		// byts, _ := json.Marshal(payload)	
-	token, _ := conf.Exchange(context.Background(), r.FormValue("code"))
-	fmt.Println(token)
-
-	// res, err := conf.Client(context.Background(), token).Get("https://discord.com/api/users/@me")
-	message, err := conf.Client(context.Background(), token).Get("https://discord.com/api/channels/1021410887191507004/messages")
-
-	ecrire, err := conf.Client(context.Background(), token).PostForm("https://discord.com/api/channels/1021410887191507004/messages", data)
-	ecrire = ecrire
-	fmt.Println("message;")
-	fmt.Println(message)
-	// byts = byts
-	if err != nil || message.StatusCode != 200 {
-		w.WriteHeader(http.StatusInternalServerError)
-		if err != nil {
-			w.Write([]byte(err.Error()))
-		} else {
-			w.Write([]byte(message.Status))
-		}
-		return
-	}
-
-	defer message.Body.Close()
-
-	body, err := ioutil.ReadAll(message.Body)
-
+	data := url.Values{}
+	data.Set("client_id", "1033382176785432656")
+	data.Set("client_secret", "XYlj-YTnAqQ-7HNes4I5xRTIdIKdLvyZ")
+	data.Set("grant_type", "authorization_code")
+	data.Set("redirect_uri", "http://localhost:8080/auth/discord")
+	data.Set("scope", "webhook.incoming")
+	data.Set("code", r.FormValue("code"))
+	encodedData := data.Encode()
+	fmt.Println(encodedData)
+	req, err := http.NewRequest("POST", myurl, strings.NewReader(encodedData))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
+		// return fmt.Errorf("Got error %s", err.Error())
 	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	response, _ := client.Do(req)
 
-	w.Write(body)
+	body, _ := ioutil.ReadAll(response.Body)
+	  
+	m := make(map[string]interface{})
+	jajerr := json.Unmarshal(body, &m)
+	if jajerr != nil {
+	    log.Fatal(jajerr)
+	}
+	address := m["webhook"].(map[string]interface{})
+	messageUrl := fmt.Sprintf("%s/%s", address["id"], address["token"])
+	webhook, err := disgohook.NewWebhookClientByToken(nil, nil, messageUrl)
+
+	Imessage, err := webhook.SendContent("premiere action bande de SCHLAGUE QUI A LA PLUS GROSSE BITE MAINTENANT HEEEEEIN @everyone\nDe la part du bot: Timothée et JUliette on des grosses quequetes")
+	webhook.SendContent("premiere action bande de SCHLAGUE QUI A LA PLUS GROSSE BITE MAINTENANT HEEEEEIN @everyone\nDe la part du bot: Timothée et JUliette on des grosses quequetes")
+
+	Imessage = Imessage
+	// Imessage, err := webhook.SendEmbeds(api.NewEmbedBuilder().
+	// SetDescription("hello world!").
+	// Build(),
+	// )
+	// Imessage, err := webhook.SendMessage(api.NewWebhookMessageCreateBuilder().
+	// 	SetContent("hello world!").
+	// 	Build(),
+	// )
+
+	// address := m["webhook"].(map[string]interface{})
+	// messageUrl := fmt.Sprintf("https://discordapp.com/api/webhooks/%s/%s", address["id"], address["token"])
+	// // const messageUrl = "https://discordapp.com/api/webhooks/" + address["id"] + "/"{address["token"];
+
+	// messageData := url.Values{}
+	// messageData.Add("content", "je suce")
+	// // messageData.Add("username", "salut lel")
+	// // messageData.Add("avatar", "")
+	// messageEncodedData := messageData.Encode()
+	// req, _ = http.NewRequest("POST", messageUrl, strings.NewReader(messageEncodedData))
+	// if err != nil {
+	// 	// return fmt.Errorf("Got error %s", err.Error())
+	// }
+	// req.Header.Add("Content-Type", "application/json")
+	// req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	// newresponse, _ := client.Do(req)
+
+	// fmt.Println(messageUrl)
+	// fmt.Println(newresponse)
+	// let body = {
+	// 	"content" : global.getParam(area.reaction.params, "message"),
+	// 	username : global.getParam(area.reaction.params, "username"),
+	// 	avatar_url : global.getParam(area.reaction.params, "avatar")
+	// };
+	// let response = await fetch(url, {
+	// 	"method": "POST",
+	// 	"body" : JSON.stringify(body),
+	// 	"headers" : {"Content-Type" : "application/json"}
+	// });
+	// res, err := conf.Client(context.Background(), token).Get("https://discord.com/api/users/@me")
+
+
+	// message, err := conf.Client(context.Background(), token).Get("https://discord.com/api/channels/1021410887191507004/messages")
+
+	// byts = byts
+	// if err != nil || res.StatusCode != 200 {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	if err != nil {
+	// 		w.Write([]byte(err.Error()))
+	// 	} else {
+	// 		w.Write([]byte(res.Status))
+	// 	}
+	// 	return
+	// }
+
+	// defer res.Body.Close()
+
+	// body, err := ioutil.ReadAll(res.Body)
+
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	w.Write([]byte(err.Error()))
+	// 	return
+	// }
+
+	// w.Write(body)
 
 }
--- web/web-app/src/Components/Discord.js
+++ web/web-app/src/Components/Discord.js
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button';
 export default function Discord() {
 const discordAuth = () => {
     console.log("bonsoir")
-    location.href = "https://discord.com/api/oauth2/authorize?client_id=1033382176785432656&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fdiscord&response_type=code&scope=identify%20email"
+    location.href = "https://discord.com/api/oauth2/authorize?client_id=1033382176785432656&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fdiscord&response_type=code&scope=webhook.incoming&permissions=536870912"
 }
 return (
     <Button variant='outlined' onClick={discordAuth}>BJR</Button>
