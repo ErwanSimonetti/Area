0b3ea2f1268c15555aa23b55e3c7e12887cd0bcf
Erwan <erwan.simonetti@epitech.eu>
2022-11-06 03:43:24 +0100

    feat(doxygen): golang functions documentation functionnal Fixes AREA-95
    It works at least partially : you can document functions (put a header
    in comment before the function that Doxygen will parse and put in the
    documentation but you I'm not sure if there is a way to put code inside
    functions yet)
    
    See the Discord channel to know exactly how to use Doxygen + go

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -1,3 +1,11 @@
+/** @file main.go
+ * @brief main file
+ *
+ * Where everything begins
+ */
+
+// @cond
+
 package main
 
 import(
@@ -8,6 +16,13 @@ import(
 	// "github.com/gofiber/fiber/middleware/cors"
 	"AREA/pkg/routes"
 )
+
+// @endcond
+
+/** main function that does xxx
+ *
+ * More detailed version (if necessary) - logs fatal for some reason
+ */
 
 func main()  {
 	r := mux.NewRouter()
