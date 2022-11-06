395851de80167ae174bc1a02beb4be323d271753
Timothée de Boynes <timothee.de-boynes@epitech.eu>
2022-11-06 18:47:16 +0100

    feat(backend): About.json
    service.son: We will need to fill this up to show all the services possible

--- backend/pkg/routes/routes.go
+++ backend/pkg/routes/routes.go
@@ -8,6 +8,8 @@ import (
 )
 
 var AreaRouter = func(router *mux.Router) {
+	router.HandleFunc("/about.json", controllers.CORS(controllers.GetAboutJson)).Methods("GET")
+
 	// router.HandleFunc("/test/", controllers.Test).Methods("POST")
 	router.HandleFunc("/register/", controllers.CORS(controllers.CreateUser)).Methods("POST")
 	// router.HandleFunc("/register/token", controllers.CreateTokenUser).Methods("POST")
