eab11f5a31220604860369f5d610a7387c1dcf91
Timothée de Boynes <timothee.de-boynes@epitech.eu>
2022-11-09 13:37:10 +0100

    feat(backend): Added correctly the delete root

--- backend/pkg/controllers/jobController.go
+++ backend/pkg/controllers/jobController.go
@@ -37,8 +37,6 @@ func RemoveJob(w http.ResponseWriter, r *http.Request) {
     }
 	models.DeleteUserJob(uint(jobId))
 	jobs.RemoveJobByID(uint(jobId))
-	w.WriteHeader(http.StatusOK)
-
 }
 
 func GetUserJobs(w http.ResponseWriter, r *http.Request) {
