9f325a178edca4eebe7f759a92bfabc32aae9021
MartinBl <martin.blancho@epitech.eu>
2022-11-13 02:00:34 +0100

    feat(front manage creation area on mobile)

--- mobile/lib/create_area_menu.dart
+++ mobile/lib/create_area_menu.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:workspace/menu_list_auth_plateform.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:grouped_buttons/grouped_buttons.dart';
 import 'package:http/http.dart' as http;
 import 'globals.dart' as globals;
 import './drop_down_menu.dart';
@@ -11,86 +13,218 @@ import 'dart:convert';
 import 'dart:io';
 import 'actions.dart';
 
-class SelectableImage extends StatelessWidget {
-  const SelectableImage({
-    Key? key,
-    required this.imageAsset,
-  }) : super(key: key);
-  final List<String> imageAsset;
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: printTextList(imageAsset),
-      ),
-    ));
-  }
-}
+// class MyDropDownMenu extends StatefulWidget {
+//   MyDropDownMenu(this._content, this._element);
+
+//   List<String> _content;
+//   String _element;
+//   List<String> _checked = [];
+
+//   List<String> get content {
+//     return _content;
+//   }
+
+//   void set content(List<String> newContent) {
+//     _content = newContent;
+//   }
+
+//   String get element {
+//     return _element;
+//   }
+
+//   void set element(String newElement) {
+//     _element = newElement;
+//   }
+
+//   // DropdownButton get button {
+//   //   return _button!;
+//   // }
+
+//   @override
+//   Widget build(context) {
+//     print(_content);
+//     print(_element);
+//     return CheckboxGroup(
+//       labels: content,
+//       disabled: [],
+//       checked: _checked,
+//       onChange: (bool isChecked, String label, int index) =>
+//           print("isChecked: $isChecked   label: $label  index: $index"),
+//       onSelected: (List<String> checked) =>
+//           print("checked: ${checked.toString()}"),
+//     );
+//   }
+// }
 
 class CreationAreaMenu extends StatefulWidget {
-  CreationAreaMenu({Key? key, this.title = ""}) : super(key: key);
-
-  final String title;
+  const CreationAreaMenu();
 
   @override
-  _CreationAreaMenuState createState() => new _CreationAreaMenuState();
+  State<CreationAreaMenu> createState() => _CreationAreaMenuState();
 }
 
 class _CreationAreaMenuState extends State<CreationAreaMenu> {
-  final _formKey = GlobalKey<FormState>();
-  String selectedValue = "Discord";
-  String selectedAction = "Appeler Erwan";
-  int currentStep = 0;
+  String actionPlateforme = "weather";
+  String reactionPlateforme = "weather";
+  // String selectedAction = "temperature_over_N_degrees";
+  int currentStep = 1;
   List<Parser> listElement = [];
+  bool haveResult = false;
+  List<String> actions = [];
+  CheckboxGroup? plateformes;
+  CheckboxGroup? plateformesActions;
+  CheckboxGroup? plateformes2;
+  CheckboxGroup? plateformesreactions;
 
+  @protected
+  @mustCallSuper
+  void initState() {}
 
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        body: Padding(
-            padding: const EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                MaterialButton(
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: () {
-                    fetchAreaction();
-                  },
-                  color: Colors.redAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40)),
-                  child: const Text(
-                    "Add an action !",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
-                    ),
-                  ),
+  @override
+  Widget build(context) {
+    if (listElement.isEmpty) {
+      fetchAreaction();
+    }
+    return Padding(
+        padding: const EdgeInsets.all(0),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          children: <Widget>[
+            Container(
+              child: plateformes,
+              decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+            ),
+            Container(
+              child: plateformesActions,
+              decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+            ),
+            Container(
+              child: plateformes2,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.green)),
+            ),
+            Container(
+              child: plateformesreactions,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.orangeAccent)),
+            ),
+            MaterialButton(
+              minWidth: double.infinity,
+              height: 60,
+              onPressed: () {
+                postArea();
+              },
+              color: Colors.redAccent,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40)),
+              child: const Text(
+                "Create Area",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
                 ),
-                Text(
-                  '+',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                  textAlign: TextAlign.center,
-                ),
-                MaterialButton(
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: () {},
-                  color: Colors.blueAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40)),
-                  child: const Text(
-                    "Add an reaction !",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ],
-            )));
+              ),
+            ),
+          ],
+        ));
+  }
+
+  void callListAction(String label) {
+    setState(() {
+      print("set state");
+      actionPlateforme = label;
+      plateformesActions = setPlateformesActions();
+    });
+  }
+
+  void callListReaction(String label) {
+    setState(() {
+      print("call list reaction");
+      reactionPlateforme = label;
+      plateformesreactions = setPlateformesReactions();
+    });
+  }
+
+  CheckboxGroup setPlateformesListAction() {
+    return CheckboxGroup(
+      labels: getPlateforme(),
+      disabled: [],
+      checked: [],
+      onChange: (bool isChecked, String label, int index) {
+        print("isChecked: $isChecked   label: $label  index: $index");
+        callListAction(label);
+      },
+      onSelected: (List<String> checked) =>
+          print("checked: ${checked.toString()}"),
+    );
+  }
+
+  CheckboxGroup setPlateformesListReaction() {
+    return CheckboxGroup(
+      labels: getPlateforme(),
+      disabled: [],
+      checked: [],
+      onChange: (bool isChecked, String label, int index) {
+        print("isChecked: $isChecked   label: $label  index: $index");
+        callListReaction(label);
+      },
+      onSelected: (List<String> checked) =>
+          print("checked: ${checked.toString()}"),
+    );
+  }
+
+  CheckboxGroup setPlateformesActions() {
+    return CheckboxGroup(
+      labels: getActionWithPlateforme(actionPlateforme),
+      disabled: [],
+      checked: [],
+      onChange: (bool isChecked, String label, int index) =>
+          print("isChecked: $isChecked   label: $label  index: $index"),
+      onSelected: (List<String> checked) =>
+          print("checked: ${checked.toString()}"),
+    );
+  }
+
+  CheckboxGroup setPlateformesReactions() {
+    print(getReactionWithPlateforme(reactionPlateforme));
+    return CheckboxGroup(
+      labels: getReactionWithPlateforme(reactionPlateforme),
+      disabled: [],
+      checked: [],
+      onChange: (bool isChecked, String label, int index) =>
+          print("isChecked: $isChecked   label: $label  index: $index"),
+      onSelected: (List<String> checked) =>
+          print("checked: ${checked.toString()}"),
+    );
+  }
+
+  // void updateContent(CheckboxGroup box, List<String> list) {
+  //   setState(() {
+  //     box. = list;
+  //   });
+  // }
+
+  Future<void> postArea() async {
+    var response =
+        await http.post(Uri.parse("http://10.0.2.2:8080/area/create"),
+            headers: {},
+            body: json.encode({
+              'action_service': '',
+              'action_func': '',
+              'action_func_params': '',
+              'reaction_service': '',
+              'reaction_func': '',
+              'reaction_func_params': '',
+            }));
+
+    if (response.statusCode == 200) {
+      Fluttertoast.showToast(msg: 'Area successfuly created');
+    } else {
+      Fluttertoast.showToast(msg: 'Error cannot created');
+    }
+
+    ///area/create
   }
 
   void iterateJson(String jsonStr) {
@@ -106,30 +240,76 @@ class _CreationAreaMenuState extends State<CreationAreaMenu> {
       for (var word in element['reactions']) {
         listReaction.add(Reac.fromJson(word));
       }
-      listElement.add(Parser(action: listAction, reaction: listReaction, plateforme: element['name']));
+      listElement.add(Parser(
+          action: listAction,
+          reaction: listReaction,
+          plateforme: element['name']));
     }
   }
 
   Future<void> fetchAreaction() async {
-    print(globals.tokenUser);
     final response = await http.get(
         Uri.parse("http://10.0.2.2:8080/area/user/propositions"),
         headers: <String, String>{
           'cookie': globals.tokenUser
         }).timeout(Duration(seconds: 30));
 
-    print('${response.statusCode}');
     if (response.statusCode == 200) {
+      haveResult = true;
       // Map<String, dynamic> myMap = json.decode(response.body);
       iterateJson(response.body);
+      setState(() {
+        plateformes = setPlateformesListAction();
+        plateformesActions = setPlateformesActions();
+        plateformes2 = setPlateformesListReaction();
+        plateformesreactions = setPlateformesReactions();
+      });
       // print(responseData);
     } else {
       Fluttertoast.showToast(msg: 'Error: Failed request');
     }
   }
+
+  List<String> getPlateforme() {
+    List<String> listPlateforme = [];
+    listElement.forEach((element) {
+      listPlateforme.add(element.plateforme);
+    });
+    print(listPlateforme.length);
+    return listPlateforme;
+  }
+
+  List<String> getActionWithPlateforme(String str) {
+    List<String> listPlateforme = [];
+    listPlateforme.clear();
+
+    listElement.forEach((element) {
+      if (element.plateforme == str) {
+        element.action.forEach((value) {
+          listPlateforme.add(value.description.toString());
+        });
+      }
+    });
+    return listPlateforme;
+  }
+
+  List<String> getReactionWithPlateforme(String str) {
+    List<String> listPlateforme = [];
+    listPlateforme.clear();
+
+    print(str);
+    listElement.forEach((element) {
+      if (element.plateforme == str) {
+        print("oui");
+        element.reaction.forEach((value) {
+          print(value.description);
+          listPlateforme.add(value.description.toString());
+        });
+      }
+    });
+    return listPlateforme;
+  }
 }
-
-
 
 List<Widget> printTextList(List<String> map) {
   List<Widget> list = [];
